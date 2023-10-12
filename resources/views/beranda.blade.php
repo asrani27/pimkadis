7b570345ca3d6747c2624a2955082ae5906ccea6
asrani <asrani.27@gmail.com>
2023-10-12 10:07:28 +0800

    f

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -122,8 +122,8 @@
               <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
               <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
               <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-youtube"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-tiktok"></i></a>
             </div>
           </div>
 
