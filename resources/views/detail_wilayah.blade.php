447e413116673cf5688d928cb0d22d56d48dd936
asrani <asrani.27@gmail.com>
2023-11-27 17:11:27 +0800

    f

--- resources/views/detail_wilayah.blade.php
+++ resources/views/detail_wilayah.blade.php
@@ -141,7 +141,7 @@
                 <div class="box-header with-border">
                   <h3 class="box-title" style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">{{strtoupper($item->nama)}} </h3>
                 </div>
-                <div class="box-body bg-green-gradient">
+                <div class="box-body bg-green-gradient text-center">
                   <h3 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif">PERSENTASE {{strtoupper($item->deskripsi)}} {{$item->value == 0 ? '0' : number_format($item->value)}} {{$item->satuan}}</h3>
 
                   <img src="/storage/{{$detail->image2}}" width="60%" height="100px">
