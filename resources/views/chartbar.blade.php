412865b92569f35099a27f8728d3834b0f92b309
asrani <asrani.27@gmail.com>
2023-11-29 08:52:46 +0800

    f

--- resources/views/chartbar.blade.php
+++ resources/views/chartbar.blade.php
@@ -100,8 +100,8 @@
               <div class="form-group">
                 <label>Jenis Chart</label>
                 <select name="jenis" class="form-control select2">
-                  <option value="pie" {{old('jenis') == 'pie' ? 'selceted':''}}>Pie Chart</option>
-                  <option value="bar" {{old('jenis') == 'bar' ? 'selceted':''}}>Bar Chart</option>
+                  <option value="pie" {{old('jenis') == 'pie' ? 'selected':''}}>Pie Chart</option>
+                  <option value="bar" {{old('jenis') == 'bar' ? 'selected':''}}>Bar Chart</option>
                 </select>
               </div>
               <button type="submit" class='btn btn-success btn-block'>TAMPILKAN</button>
